5502a3fbb67ec27d76ce6d23917c411c7389930e
jasonjoby <jason.joby072@gmail.com>
2021-09-06 23:36:20 +0530

    Scrollbar and BTT into styleSB.css and main.js

--- wiki/src/js/main.js
+++ wiki/src/js/main.js
@@ -16,3 +16,46 @@ if (window.location.href.includes('igem.org')) {
     }
     document.querySelectorAll('.mw-body')[0].removeAttribute('id')
 }
+
+//////////// Back to top button @Namasi22
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 470 || document.documentElement.scrollTop > 470) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 450;
+  document.documentElement.scrollTop = 0;
+}
+
+//////////// Scroll Indicator @Namasi22
+
+	window.addEventListener('scroll',
+			moveScrollIndicator);
+
+	const scrollIndicatorElt =
+		document.getElementById('scrollIndicator');
+
+	const maxHeight =
+		window.document.body.scrollHeight
+		- window.innerHeight;
+
+	function moveScrollIndicator(e) {
+		const percentage = 
+			((window.scrollY) / maxHeight) * 50;
+
+		scrollIndicatorElt.style.height
+			= percentage + '%';
+	}
+//////////// End scrollbar
